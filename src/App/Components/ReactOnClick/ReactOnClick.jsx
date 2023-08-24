4b76056cdbaa007a6c93bcd65fceef1202b0262c
Milena <salekmilena@gmail.com>
2023-08-24 20:38:35 +0200

    onclick2

--- src/App/Components/ReactOnClick/ReactOnClick.jsx
+++ src/App/Components/ReactOnClick/ReactOnClick.jsx
@@ -2,19 +2,20 @@ import { useState } from 'react';
 import './styles.css';
 
 export const ReactOnClick = () => {
-  const [isClicked, setIsClicked] = useState(false);
-
-  const text = isClicked ? 'Klikniete' : 'Nie klikniete';
+  // const [isClicked, setIsClicked] = useState(false);
+  const [count, setCount] = useState(0);
+  // const text = isClicked ? 'Klikniete' : 'Nie klikniete';
 
   function handleClick() {
-    setIsClicked(!isClicked);
+    // setIsClicked(!isClicked);
+    setCount(count + 1);
   }
 
   return (
     <div>
       <h1>Cześć!</h1>
 
-      <button onClick={handleClick}>{text}</button>
+      <button onClick={handleClick}>{count}</button>
     </div>
   );
 };

d2e1edd8597bafd508328c366c782702779b9667
Milena <salekmilena@gmail.com>
2023-08-28 11:58:53 +0200

    onclick3

--- src/App/Components/ReactOnClick/ReactOnClick.jsx
+++ src/App/Components/ReactOnClick/ReactOnClick.jsx
@@ -11,6 +11,19 @@ export const ReactOnClick = () => {
   function handleChange(event) {
     setValue(event.target.value);
   }
+
+  const test = 'Tom';
+
+  if (test === 'Sam') {
+    console.log('Test jest równe Sam');
+  }
+  if (test === 'Tom') {
+    console.log('Test jest równe Tom');
+  }
+  if (typeof test === 'string') {
+    console.log('Test jest stringiem');
+  }
+
   return (
     <div>
       <h1>Cześć!</h1>

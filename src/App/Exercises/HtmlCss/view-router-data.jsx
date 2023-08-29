793043f8cdb821ac6412bbfc3486f8e16929ec36
Milena <salekmilena@gmail.com>
2023-08-29 19:00:20 +0200

    TextFundaments 29.08.2023

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,6 +1,10 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { htmlCssblockRouterMetaData } from './SelectorsAndCascade/router-data';
+import { htmTextFundamentsRouterMetaData } from './TextFundaments/router-data';
 
-export const blockRouterMetaData = [htmlCssblockRouterMetaData];
+export const blockRouterMetaData = [
+  htmlCssblockRouterMetaData,
+  htmTextFundamentsRouterMetaData,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);

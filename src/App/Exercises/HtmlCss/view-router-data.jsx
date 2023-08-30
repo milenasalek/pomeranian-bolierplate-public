9320f1e1a42d449c560242d250964b6afa74d6d5
Milena <salekmilena@gmail.com>
2023-08-30 18:16:29 +0200

    CssAnimations 30.08.2023

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,10 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { htmlCssRouterMetaData } from './SelectorsAndCascade/router-data';
 import { textFundamentsRouterMetaData } from './TextFundaments/router-data';
 import { standardTagsRouterMetaData } from './StandardTags/router-data';
+import { CssAnimationsRouterMetaData } from './CssAnimations/router-data';
 
 export const blockRouterMetaData = [
   htmlCssRouterMetaData,
   textFundamentsRouterMetaData,
   standardTagsRouterMetaData,
+  CssAnimationsRouterMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);

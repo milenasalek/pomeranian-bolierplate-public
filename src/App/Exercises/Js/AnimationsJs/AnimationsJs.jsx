f8517c00512883b385e8ab4ed6506e80071201e3
Milena <salekmilena@gmail.com>
2023-09-18 16:50:15 +0200

    AnimationsJs

--- src/App/Exercises/Js/AnimationsJs/AnimationsJs.jsx
+++ src/App/Exercises/Js/AnimationsJs/AnimationsJs.jsx
@@ -1,3 +1,31 @@
+import { useEffect, useState } from 'react';
 import './styles.css';
 
-export const AnimationsJs = () => {};
+export const AnimationsJs = () => {
+  const [text, setText] = useState('I love react :)');
+  const [isAnimated, setIsAnimated] = useState(false);
+  const [opacity, setOpacity] = useState(1);
+  const toggleButton = () => {
+    setIsAnimated((prev) => !prev);
+  };
+
+  useEffect(() => {
+    // tutaj logika
+  }, [text]);
+
+  useEffect(() => {
+    if (opacity > 0) {
+      const interval = setInterval(() => {
+        setOpacity(opacity - 0.005);
+      }, 50);
+      return () => clearInterval(interval);
+    }
+  }, [opacity]);
+  return (
+    <div>
+      <p>{text}</p>
+      <button onClick={toggleButton}>{isAnimated ? 'Off' : 'On'}</button>
+      <div className="animated-box" style={{ opacity: opacity }}></div>
+    </div>
+  );
+};
